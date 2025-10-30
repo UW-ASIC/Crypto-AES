52022bb3cedc84f04e2a8ff5e4ddafde8fa6b1b8
judywsl <j542yu@uwaterloo.ca>
2025-10-30 15:59:38 -0400

    Change subbytes to time-multiplexed logic (#4)
    * To reduce hardware usage, 8 bytes of data will be
      passed in each clock cycle for 16 total cycles
      (plus one extra cycle for `state_out` to stabilize)
    
    * In top level module that instantiates subbytes,
      `start` should be asserted for one cycle to begin
      the subbytes operation
    
    * `state_in` must be provided when asserting `start`
      because `byte_in` loads in first byte of `state_in`
      in same clock cycle that `start` is high

--- src/subbytes.v
+++ src/subbytes.v
@@ -1,21 +1,76 @@
 `default_nettype none
 
 module subbytes (
+    input wire clk,
+    input wire rst_n,
+    input wire start, // Asserted for one clock cycle when `state_in` is ready
     input  wire [127:0] state_in,
-    output wire [127:0] state_out
+    output reg [127:0] state_out,
+    output reg done
 );
 
-    // replace each byte in state_in using
-    // corresponding sbox values
-    genvar i;
-    generate
-        for ( i = 0; i < 16; i = i + 1 )
-        begin
-            sbox sbox_inst( 
-                state_in[8*i +: 8], 
-                state_out[8*i +: 8] 
-            );
+    // To reduce hardware usage, 8 bytes of data will be
+    // passed in each clock cycle for 16 total cycles
+    // (plus one extra cycle for `state_out` to stabilize)
+    //
+    // In top level module that instantiates subbytes,
+    // `start` should be asserted for one cycle to begin 
+    // the subbytes operation
+    //
+    // `state_in` must be provided when asserting `start`
+    // because `byte_in` loads in first byte of `state_in` in same
+    // clock cycle that `start` is high
+
+    reg started; // Keep track of if `start` has been asserted
+    reg [3:0] current_clock_cycle; // 0 to 15
+
+    reg [7:0] byte_in;
+    reg [7:0] byte_out;
+
+    // sbox is combinational and updates instantenously
+    sbox sbox_inst(byte_in, byte_out);
+
+    always @(posedge clk or negedge rst_n) 
+    begin
+        if(!rst_n) begin
+            started <= 0;
+            done <= 0;
+
+            current_clock_cycle <= 0;
+            
+            byte_in <= '0;
+            // byte_out is determined by sbox
+
+            state_out <= '0;
+        end else begin
+            if (start) begin
+                started <= 1;
+                done <= 0;
+
+                byte_in <= state_in[7:0];
+
+                state_out <= '0;
+
+                current_clock_cycle <= 0;
+            end
+
+            // For each cycle, byte_in for the next cycle will be calculated
+            // and state_out for the current cycle will be updated to
+            // accomodate for non-blocking assignment timing delays
+            if (started) begin 
+                state_out[8*current_clock_cycle +: 8] <= byte_out; 
+
+                if (current_clock_cycle == 4'b1111) begin
+                    done <= 1;
+                    started <= 0;
+
+                end else begin
+                    byte_in <= state_in[8*(current_clock_cycle + 1) +: 8];
+                end
+
+                current_clock_cycle <= current_clock_cycle + 1;
+            end
         end
-    endgenerate
+    end
 
 endmodule
--- test/test_subbytes.py
+++ test/test_subbytes.py
@@ -1,36 +1,57 @@
 import cocotb
-from cocotb.triggers import Timer  
-from cocotb.types import Range
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
 
-async def run_subbytes_case(dut, state_in, state_out):
+async def run_subbytes_case(clk, dut, state_in, state_out):
+    dut.start.value = 1
+
+    # `byte_in`` is loaded before `started` is asserted,
+    # so `state_in` must be inputted alongside `start`
     dut.state_in.value = state_in
-    # No clock cycle because combinational logic only
-    await Timer(1, "ns")
+
+    await ClockCycles(clk, 1)
+
+    dut.start.value = 0
+
+    # Must wait 16 cycles for computation to complete
+    # and 1 extra cycle for `state_out` to stabilize
+    await ClockCycles(clk, 17)
     assert dut.state_out.value == state_out, f"Expected {state_out}, received {dut.state_out.value}"
 
 @cocotb.test()
 async def test_subbytes(dut):
     dut._log.info("Start subbytes testing")
 
-    # No reset because combinational logic only
+    # Set the clock period to 100 ns (10 MHz)
+    clock = Clock(dut.clk, 100, units="ns")
+    cocotb.start_soon(clock.start())    
+
+    dut._log.info("Reset")
+    
+    dut.rst_n.value = 0
+    dut.start.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
+    dut._log.info("Test project behavior")
 
     #--------------------------------------------------------------------------------#
 
     dut._log.info("Test with all 0's in state_in value")
 
-    await run_subbytes_case(dut, 0, 0x63636363636363636363636363636363)
+    await run_subbytes_case(dut.clk, dut, 0, 0x63636363636363636363636363636363)
 
     #--------------------------------------------------------------------------------#
 
     # LogicArray type allows bitwise operations
     dut._log.info("Test with all 1's in state_in value")
 
-    await run_subbytes_case(dut, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF, 0x16161616161616161616161616161616)
+    await run_subbytes_case(dut.clk, dut, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF, 0x16161616161616161616161616161616)
 
     #--------------------------------------------------------------------------------#
 
     dut._log.info("Test with known values from FIPS 197 AES standard (Appendix B, rounds 1 to 3)")
 
-    await run_subbytes_case(dut, 0x193de3bea0f4e22b9ac68d2ae9f84808, 0xd42711aee0bf98f1b8b45de51e415230)
-    await run_subbytes_case(dut, 0xa49c7ff2689f352b6b5bea43026a5049, 0x49ded28945db96f17f39871a7702533b)
-    await run_subbytes_case(dut, 0xaa8f5f0361dde3ef82d24ad26832469a, 0xac73cf7befc111df13b5d6b545235ab8)
+    await run_subbytes_case(dut.clk, dut, 0x193de3bea0f4e22b9ac68d2ae9f84808, 0xd42711aee0bf98f1b8b45de51e415230)
+    await run_subbytes_case(dut.clk, dut, 0xa49c7ff2689f352b6b5bea43026a5049, 0x49ded28945db96f17f39871a7702533b)
+    await run_subbytes_case(dut.clk, dut, 0xaa8f5f0361dde3ef82d24ad26832469a, 0xac73cf7befc111df13b5d6b545235ab8)
