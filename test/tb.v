be0935295a9b26a59e479f9fdb9aa866c77d375b
judywsl <j542yu@uwaterloo.ca>
2025-11-05 09:55:12 -0500

    Add Makefile and tb.v for Github Actions

--- test/tb.v
+++ test/tb.v
@@ -16,34 +16,31 @@ module tb ();
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  reg start;
+  reg advance;    // advance one round
+  reg [3:0] round;
+  reg is_final;
+  reg done;
+
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  round_counter user_project (
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+    // Include power ports for the Gate Level test:
+    `ifdef GL_TEST
+          .VPWR(VPWR),
+          .VGND(VGND),
+    `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+    .clk(clk),
+    .rst_n(rst_n),
+    .advance(advance),
+    .round(round),
+    .is_final(is_final),
+    .done(done)
   );
 
 endmodule
