72437af4a0dd1ce5e860aa880cc2949a9b6f2c77
sata4949 <sata@tutamail.com>
2026-03-18 16:51:50 -0400

    optimize roundkey_gen, fix linter warning

--- src/sbox.v
+++ src/sbox.v
@@ -170,7 +170,7 @@ module sbox ( byte_in, byte_out );
     output [7:0] byte_out;
     wire [7:0] B, C, D, Z; // X, Y,
     wire R1, R2, R3, R4, R5, R6, R7, R8, R9;
-    wire T1, T2, T3, T4, T5, T6, T7, T8, T9, T10;
+    wire T1, T2, T3, T4, T5, T6, T7, T8, T9;//, T10;
 
  /* change basis from GF(2^8) to GF(2^8)/GF(2^4)/GF(2^2) */
  /* combine with bit inverse matrix multiply of Sbox */
